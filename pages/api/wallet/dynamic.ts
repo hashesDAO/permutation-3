a3e729a79cc4bc456373cb4f65c4195ea4467c66
mathesond2 <mathesond2@gmail.com>
2022-06-08 22:15:35 -0400

    add logic for /wallet/dynamic

--- pages/api/wallet/dynamic.ts
+++ pages/api/wallet/dynamic.ts
@@ -1,12 +1,93 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { ethers } from 'ethers';
+import {
+  getHashesContract,
+  getHashesDAOContract
+ } from '../../../util';
 
-type Data = {
-  name: string
+type WalletHash = {
+  hash_value: string
+  type: 'DAO' | 'DAO Deactivated' | 'Standard' //TODO: make as enum
+  minted_by_address: boolean
+  blocks_held: number
 }
 
-export default function handler(
+type ResponseData = {
+  hashes: WalletHash[]
+  on_chain_votes: number
+  owns_perm_2_nft: boolean
+};
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<ResponseData | string | any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { address } = req.query;
+
+  if (
+    !address ||
+    typeof(address) !== 'string' ||
+    !ethers.utils.isAddress(address)
+  ) {
+    res.status(400).send('valid (non-ens) wallet address must be provided');
+    return;
+  }
+
+  const hashesContract = getHashesContract(1);
+  const hashesBalance = await hashesContract.balanceOf(address);
+  const hashCount = hashesBalance.toNumber();
+
+  if (hashCount === 0) {
+    res.status(404).send('wallet does not have a hash token');
+    return;
+  }
+
+  const etherscanProvider = new ethers.providers.EtherscanProvider(1, process.env.ETHERSCAN_API_KEY);
+  const currentBlockNumber = await etherscanProvider.getBlockNumber();
+
+  const hashes = [];
+  for (let i = 0; i < hashCount; i++) {
+    const tokenId = await hashesContract.tokenOfOwnerByIndex(address, i);
+    const [hash, isDeactivated]: [string, boolean] = await Promise.all([
+      hashesContract.getHash(tokenId),
+      hashesContract.deactivated(tokenId),
+    ]);
+
+    const generatedFilter = hashesContract.filters.Generated();
+    const AllGeneratedEvents = await hashesContract.queryFilter(generatedFilter);
+    const tokenIdEvent = AllGeneratedEvents.find(event => Number(event?.args?.tokenId) === Number(tokenId));
+
+    const artist = tokenIdEvent?.args?.artist ? tokenIdEvent?.args?.artist : null;
+    const blocksHeld = tokenIdEvent?.blockNumber ? currentBlockNumber - tokenIdEvent?.blockNumber : 0;
+    //TODO: put into util fn
+    const type = Number(tokenId) >= 1000 ? 'Standard' : isDeactivated ? 'DAO Deactivated' : 'DAO';
+
+    hashes.push({
+      hash_value: hash,
+      type,
+      minted_by_address: artist === address,
+      blocks_held: blocksHeld,
+    });
+  };
+
+  const hasNonStandardHash = hashes
+    ?.map(hash => hash.type)
+    .some(type => type === 'DAO' || type === 'DAO Deactivated');
+
+  let votesCastByAddress = 0;
+
+  if (hasNonStandardHash) {
+    const hashesDAOContract = getHashesDAOContract();
+    const voteCastFilter = hashesDAOContract.filters.VoteCast();
+    const allVoteCastEvents = await hashesDAOContract.queryFilter(voteCastFilter);
+
+    votesCastByAddress = allVoteCastEvents
+      .filter(event => event?.args?.voter.toLowerCase() === address.toLowerCase())
+      .length;
+  }
+
+  res.status(200).json({
+    hashes,
+    on_chain_votes: votesCastByAddress,
+  });
 }
--- util/index.ts
+++ util/index.ts
@@ -1,5 +1,6 @@
 import { ethers } from 'ethers';
 import HASHES_ABI from './Hashes.json';
+import HASHES_DAO_ABI from './HashesDAO.json';
 
 export function hex2bin(hex: string) {
   return hex
@@ -27,9 +28,21 @@ export const HASHES_ADDRESS = {
   [ChainId.RINKEBY]: '0x25ca4f68d0f8271b0796406101ad6430a149af17',
 };
 
+export const HASHESDAO_ADDRESS = {
+  [ChainId.MAINNET]: '0xbD3Af18e0b7ebB30d49B253Ab00788b92604552C',
+  [ChainId.KOVAN]: '0xb2c0ADD2C81732A5D6609d122340fBafF99D7cC1',
+};
+
 export function getHashesContract(chain_id: number | undefined): ethers.Contract {
   const chainId = 1; //temp
   const provider = new ethers.providers.InfuraProvider(INFURA_PREFIXES[chainId]);
   const newContract =  new ethers.Contract(HASHES_ADDRESS[chainId], HASHES_ABI.abi, provider);
+  return newContract;
+}
+
+export function getHashesDAOContract(): ethers.Contract {
+  const chainId = 1; //temp
+  const provider = new ethers.providers.InfuraProvider(INFURA_PREFIXES[chainId]);
+  const newContract =  new ethers.Contract(HASHESDAO_ADDRESS[chainId], HASHES_DAO_ABI.abi, provider);
   return newContract;
 }
