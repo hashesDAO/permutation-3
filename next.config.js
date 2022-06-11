956e45517b47fd8d942bbac55f03f63eea0f4e67
mathesond2 <mathesond2@gmail.com>
2022-06-10 20:20:24 -0400

    add 2 modules, modify next.config to use swagger-ui-react in app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const withTM = require('next-transpile-modules')([
+  'swagger-ui-react',
+  'react-syntax-highlighter',
+  'swagger-client'
+]);
+
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
