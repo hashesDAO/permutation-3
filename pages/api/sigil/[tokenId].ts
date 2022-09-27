1942df984b8ee93d3678eb32e08d450e54b04a1d
william.ridge.95 <william.ridge.95@gmail.com>
2022-09-27 15:42:12 +1000

    refining

--- pages/api/sigil/[tokenId].ts
+++ pages/api/sigil/[tokenId].ts
@@ -1,4 +1,4 @@
-import { getHashesCollectionContract } from '../../../util';
+import { getHashesCollectionContract, getHashesContract } from '../../../util';
 import Addresses from '../../../addresses.json';
 
 //http://localhost:3000/api/sigil
@@ -12,11 +12,18 @@ type ProcessedTokenData = {
 type ProcessedWalletData = {
   dao: number
   non_dao: number
+  votes: number
+  proposals: number
 };
 
 type ColourPalette = {
   name: string
   colours: string[]
+}
+
+type FontData = {
+  font: string
+  fontsize: string
 }
 
 //Gets the Token Hashes data from pre-existing API
@@ -74,12 +81,10 @@ function processWalletAPIData(data: any): ProcessedWalletData {
   const processedWalletData = {} as ProcessedWalletData;
 
   try {
-
     //WalletData will return an undefined if the owner does not own any Hashes NFTs
     if (data === undefined) {
 
       processedWalletData.dao = 0;
-
       processedWalletData.non_dao = 0;
     }
     else {
@@ -89,6 +94,7 @@ function processWalletAPIData(data: any): ProcessedWalletData {
 
       for (let i = 0; i < data.hashes.length; i++) {
 
+        //Adds entry to either DAO or non-DAO increment
         if (data.hashes[i].type === "DAO") {
           dao += 1;
         }
@@ -98,50 +104,144 @@ function processWalletAPIData(data: any): ProcessedWalletData {
       }
 
       processedWalletData.dao = dao;
-
       processedWalletData.non_dao = non_dao;
     }
   } catch (error) {
-    console.error(`error transposing wallet data: ${error}`);
+    console.error(`error transposing wallet data: dao/non-dao : ${error}`);
+  }
+
+  try {
+    //WalletData will return an undefined if the owner does not own any Hashes NFTs
+    if (data === undefined) {
+      processedWalletData.votes = 0;
+      processedWalletData.proposals = 0;
+    }
+    else {
+      processedWalletData.votes = data.on_chain_votes;
+      processedWalletData.proposals = data.proposals_created;
+    }
+  } catch (error) {
+    console.error(`error transposing wallet data: snapshot data ${error}`);
   }
 
   return processedWalletData;
 }
 
-//Need to re-do how this function works
-function getColourPalette(seed: number): ColourPalette {
+//Not sure I need this - Yeah I think I don't
+function isOwnerMinter(owner: string, minter: string): boolean {
+  if(owner === minter) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-  const colourPalette = {} as ColourPalette;
+//Returns a true if owner of sigil also owns the Hashes hash used to mint the sigil
+function isConnected(ownerOfSigil:string, ownerOfMintingHash: string): boolean {
+  if(ownerOfSigil === ownerOfMintingHash) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-  //Change later
-  const names = ["mono", "tyler", "Cerniak", "Cooki", "Deebee", "DMath", "Binmaker"];
-  const mono = ["#080808", "#222222", "#616161", "#AAAAAA", "#ECECEC"];
-  const tyler = ["#d12a2f", "#315f8c", "#3b2b20", "#b8d9ce", "#ebe4d8"];
+//Gets the colour palette given the seed and whether or not the sigil is connected to the minting hash
+function getColourPalette(seed: number, isConnected: boolean): ColourPalette {
 
-  const palettes = [mono, tyler];
+  const mono = {name: "Mono", colours: ["#080808", "#222222", "#616161", "#aaaaaa", "#ececec"]} as ColourPalette;
 
-  colourPalette.colours = palettes[seed % palettes.length];
-  colourPalette.name = names[seed % names.length];
+  //If sigil is disconnected from minting hashes hash return mono palette
+  if (isConnected == false) {
+    return mono;
+  }
 
-  return colourPalette;
+  //The other palettes
+  const tyler = {name: "Tyler", colours: ["#d12a2f", "#315f8c", "#3b2b20", "#b8d9ce", "#ebe4d8"]} as ColourPalette;
+  const kevin = {name: "Kevin", colours: ["#f524d7", "#000000", "#303841", "#f1b72c", "#f50f1c"]} as ColourPalette;
+  const x = {name: "X", colours: ["#050615", "#9ae8af", "#7138ca", "#f34ca2", "#3a1e6d"]} as ColourPalette;
+  const sol = {name: "Sol", colours: ["#332f2a", "#e40643", "#ffce04", "#0167a4", "#f0efe7"]} as ColourPalette;
+  const herbert = {name: "Herbert", colours: ["#4d334e", "#df327c", "#7b1a45", "#88ffff", "#f2ffff"]} as ColourPalette;
+  const rhea = {name: "Rhea", colours: ["#012041", "#6001c0", "#4060c1", "#a1e101", "#e0a17f"]} as ColourPalette;
+  const OP6334 = {name: "Open palette 6334", colours: ["#339911", "#ddbbbb", "#0044ff", "#ddee11", "#ddffff"]} as ColourPalette;
+  const VPJ = {name: "Vitalik's pyjamas", colours: ["#ff534d", "#9d0107", "#a54100", "#ffc8ae", "#bfa7a1"]} as ColourPalette;
+
+  const standardPalettes = [tyler, kevin, x, sol, herbert, rhea, OP6334];
+
+  //~3% chance of getting Vitalik's PJs
+  if (seed % 33 == 0) {
+    return VPJ;
+  }
+  //else a uniform chance of getting any other standard pallette
+  else {
+    return standardPalettes[(seed % standardPalettes.length)];
+  }
+
+  //There is a slight probability collision given the multiples of 33 and 7 but whatever, it's not a big deal
+}
+
+//Gets the colours used - updates with the number of hashes owned
+function getColoursUsed(seed: number, daohashes: number, nondaohashes: number): number[] {
+
+  const coloursUsed = [0,1];
+
+  //Background colour dependent on total number of hashes owned
+  coloursUsed[0] = (daohashes + nondaohashes) % 5;
+
+  //Text colour dependent on total number of hashes owned + seed
+  coloursUsed[1] = (seed + daohashes + nondaohashes) % 5;
+
+  //makes sure the same colours aren't used
+  if (coloursUsed[0] == coloursUsed[1]) {
+    coloursUsed[1] = (seed + daohashes + nondaohashes + 1) % 5;
+  }
+
+  return coloursUsed;
 }
 
 function getFont(seed: number): string {
 
   const fonts = ["Ropa+Sans", "Courier Prime", "Oswald", "Poppins", "Roboto Condensed", "Montserrat", "Bebas Neue", "Prompt", "Space Grotesque", "Righteous", "Archivo Black", "Tavaraj", "Gruppo", "Linden Hill"];
 
-  return fonts[seed % fonts.length];
+  return fonts[(seed % fonts.length)];
+}
+
+//Gets the phrase - cut down if it's too long
+function getPhrase(phrase: string): string {
+
+  if (phrase == null || undefined) {
+    return "";
+  }
+
+  if (phrase.length < 72) {
+    return phrase;
+  }
+  //Cut to prevent excessively long string
+  else {
+    var tempPhrase = "";
+    
+    tempPhrase = phrase.slice(0,72);
+
+    tempPhrase = tempPhrase.concat("...");
+
+    return tempPhrase;
+  }
 }
 
 //Returns the SVG image for the NFT
-function getSigilBase64EncodedSVG(hashesTokenId: number, seed: number, processedTokenData: ProcessedTokenData, processedWalletData: ProcessedWalletData): string {
+function getSigilBase64EncodedSVG(hashesTokenId: number, isConnected: boolean, seed: number, processedTokenData: ProcessedTokenData, processedWalletData: ProcessedWalletData): string {
 
   //Size and positioning controlled with these variables
   const xdimension = 260;
   const ydimension = 350;
 
-  //Gets the colour palette given the random seed (i.e. the hashes hash)
-  const colourPalette = getColourPalette(seed);
+  //Gets the colour palette given the random seed (i.e. the hashes hash) and whether or not the sigil owner also owns the minting hash
+  const colourPalette = getColourPalette(seed, isConnected);
+
+  //The two colours used: dependent on the number of dao and non-dao hashes owned in the wallet
+  //0: background colour, 1: font colour
+  const coloursUsed = getColoursUsed(seed, processedWalletData.dao, processedWalletData.non_dao);
 
   //Gets the font given the random seed
   //Also needs to be amenable to snapshot variables
@@ -150,20 +250,8 @@ function getSigilBase64EncodedSVG(hashesTokenId: number, seed: number, processed
   //Hashes Id that minted the sigil
   const HashesID = `Token ID: ${hashesTokenId}`;
 
-  //Cut to prevent excessively long string
-  var Phrase = "";
-  if (processedTokenData.phrase_value == null || undefined) {
-  }
-  else {
-    Phrase = processedTokenData.phrase_value.slice(0,70);
-  }
-
-  //The binary string of the hash - broken into four for display purposes
-  //Lose the binary
-  const Binary0 = processedTokenData.binary_value.slice(0,64);
-  const Binary1 = processedTokenData.binary_value.slice(64,128);
-  const Binary2 = processedTokenData.binary_value.slice(128,192);
-  const Binary3 = processedTokenData.binary_value.slice(192,256);
+  //The hashes phrase
+  const Phrase = getPhrase(processedTokenData.phrase_value);
 
   //Begins and sets the dimensions
   var svgHTML = `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 ${xdimension} ${ydimension}">`;
@@ -174,25 +262,28 @@ function getSigilBase64EncodedSVG(hashesTokenId: number, seed: number, processed
   svgHTML = svgHTML.concat(`<defs><style type="text/css">@import url('https://fonts.googleapis.com/css?family=Ropa+Sans|Courier+Prime|Oswald|Poppins|Roboto+Condensed|Montserrat|Bebas+Neue|Prompt|Space+Grotesque|Righteous|Archivo+Black|Tavaraj|Gruppo|Linden+Hill');</style></defs>`);
 
   //Sets the main0 font style: colour, font, and font size
-  svgHTML = svgHTML.concat(`<style>.main0 { fill: ${colourPalette.colours[0]}; font-family: ${font}; font-size: 80px; text-anchor: middle }</style>`);
+  svgHTML = svgHTML.concat(`<style>.main0 { fill: ${colourPalette.colours[coloursUsed[1]]}; font-family: ${font}; font-size: 80px; text-anchor: middle }</style>`);
 
   //Sets the main1 font style: colour, font, and font size
-  svgHTML = svgHTML.concat(`<style>.main1 { fill: ${colourPalette.colours[0]}; font-family: ${font}; font-size: 20px; text-anchor: middle }</style>`);
+  svgHTML = svgHTML.concat(`<style>.main1 { fill: ${colourPalette.colours[coloursUsed[1]]}; font-family: ${font}; font-size: 20px; text-anchor: middle }</style>`);
 
   //Sets the support0 font style: colour, font, and font size
-  svgHTML = svgHTML.concat(`<style>.support0 { fill: ${colourPalette.colours[0]}; font-family: courier; font-size: 12px; text-anchor: middle }</style>`);
+  svgHTML = svgHTML.concat(`<style>.support0 { fill: ${colourPalette.colours[coloursUsed[1]]}; font-family: courier; font-size: 10px; text-anchor: middle }</style>`);
 
   //Sets the support1 font style: colour, font, and font size
-  svgHTML = svgHTML.concat(`<style>.support1 { fill: ${colourPalette.colours[0]}; font-family: courier; font-size: 6px; text-anchor: middle }</style>`);
+  svgHTML = svgHTML.concat(`<style>.support1 { fill: ${colourPalette.colours[coloursUsed[1]]}; font-family: courier; font-size: 6px; text-anchor: middle }</style>`);
 
   //Backgound
 
   //Sets the background colour
-  svgHTML = svgHTML.concat(`<rect width="100%" height="100%" fill= "${colourPalette.colours[4]}" />`);
+  svgHTML = svgHTML.concat(`<rect width="100%" height="100%" fill= "${colourPalette.colours[coloursUsed[0]]}" />`);
 
   //Main Text
 
-  //Whale: (__-){
+  //DAO Whale: (__-){
+  //Non-DAO Whale: .___.
+
+  //This needs polishing with formatting/location anyway
   
   //Sets the main0 text - the DAO hash # characters
   //Number of strings depends on the number of DAO hashes owned
@@ -363,28 +454,16 @@ function getSigilBase64EncodedSVG(hashesTokenId: number, seed: number, processed
 
   //Supporting Text
 
-  //Sets the support0 text - the token Id of the hashes NFT used to mint
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6)}" class="support0">${HashesID}</text>`);
+  //ydimension - (ydimension/36)
+
+  //Sets the support0 text - the token Id of the hashes NFT used to mint - made bold
+  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${ydimension - 4 * (ydimension/36)}" font-weight="bold" class="support0">${HashesID}</text>`);
 
   //Sets the first support1 text - the hashes phrase
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6) + (ydimension/36)}" class="support1">${Phrase}</text>`);
+  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${ydimension - 3 * (ydimension/36)}" class="support1">${Phrase}</text>`);
 
   //Sets the second support1 text - the hashes hash
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6) + 2 * (ydimension/36)}" class="support1">${processedTokenData.hash}</text>`);
-
-  //Binaries (Broken into 4 equal lengths of 64)
-
-  //Sets the second support1 text - the binary0
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6) + 3 * (ydimension/36)}" class="support1">${Binary0}</text>`);
-
-  //Sets the second support1 text - the binary1
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6) + 3 * (ydimension/36) + (ydimension/60)}" class="support1">${Binary1}</text>`);
-
-  //Sets the second support1 text - the binary2
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6) + 3 * (ydimension/36) + 2 * (ydimension/60)}" class="support1">${Binary2}</text>`);
-
-  //Sets the second support1 text - the binary3
-  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${2 * (ydimension/3) + (ydimension/6) + 3 * (ydimension/36) + 3 * (ydimension/60)}" class="support1">${Binary3}</text>`);
+  svgHTML = svgHTML.concat(`<text x="${xdimension/2}" y="${ydimension - 2 * (ydimension/36)}" class="support1">${processedTokenData.hash}</text>`);
 
   //Ends it and wraps up
   svgHTML = svgHTML.concat(`</svg>`);
@@ -409,6 +488,9 @@ export default async function handler(req: any, res: any) {
   //Sigils collection address
   const sigilV0Contract = getHashesCollectionContract(Addresses.sigilV0CollectionAddress_currentlyMedleyLimited);
 
+  //Hashes address
+  const hashesContract = getHashesContract(1);
+
   //Gets the address of the owner of the sigil (tokenId)
   const owner = await sigilV0Contract.ownerOf(tokenId);
 
@@ -424,6 +506,19 @@ export default async function handler(req: any, res: any) {
   //Converts to base 10
   const hashesTokenId = parseInt(rawHashesTokenId, 16);
 
+  //Not sure I need this actually?
+  //Also gets the minting address
+  const minter = tokenIdEvent?.args?.minter;
+  //Checks if minter is still the owner
+  const isMinter = isOwnerMinter(owner, minter);
+
+  //Also gets the current owner of the minting Hashes NFT
+  const ownerOfMintingHash = await hashesContract.ownerOf(hashesTokenId);
+  //Boolean whether or not the owner of the sigil is also the owner of the Hashes NFT that was used to mint
+  const isSigilConnected = isConnected(owner, ownerOfMintingHash);
+
+  //Parallelise this data fetching
+
   //Gets the relevant token data from pre-existing API
   const tokenData = await getTokenAPIData(hashesTokenId);
 
@@ -436,10 +531,6 @@ export default async function handler(req: any, res: any) {
   //Processes the revelant owner data for easy use
   const processedWalletData = processWalletAPIData(walletData);
 
-  //console.log(processedTokenData);
-
-  //console.log(processedWalletData);
-
   //The seed for randomness derived from the hashes NFT hash
   const seed = Number(processedTokenData.hash);
 
@@ -447,39 +538,15 @@ export default async function handler(req: any, res: any) {
   res.status(200).json({
     name: `Sigil #${tokenId}`,
     description: 'TODO',
-    image: `data:image/svg+xml;base64,${getSigilBase64EncodedSVG(hashesTokenId, seed, processedTokenData, processedWalletData)}`,
+    image: `data:image/svg+xml;base64,${getSigilBase64EncodedSVG(hashesTokenId, isSigilConnected, seed, processedTokenData, processedWalletData)}`,
     attributes: [
-        {
-            trait_type: 'DAO Hashes Owned',
-            value: processedWalletData.dao,
-        },
-        {
-          trait_type: 'Non-DAO Hashes Owned',
-          value: processedWalletData.non_dao,
-        },
-        {
-          trait_type: 'Minted With',
-          value: hashesTokenId,
-        },
-        {
-          trait_type: 'Phrase',
-          value: processedTokenData.phrase_value,
-        },
-        {
-          trait_type: 'Hash',
-          value: processedTokenData.hash,
-        },
-        {
-          trait_type: 'Binary',
-          value: processedTokenData.binary_value,
-        },
         {
           trait_type: 'Font',
           value: getFont(seed),
         },
         {
-          trait_type: 'ColourPalette',
-          value: getColourPalette(seed).name,
+          trait_type: 'Colour Palette',
+          value: getColourPalette(seed, isSigilConnected).name,
         },
     ],
   });
