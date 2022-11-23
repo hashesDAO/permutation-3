db0d4313a52c52aa9be46885872447da93aa0be7
william.ridge.95 <william.ridge.95@gmail.com>
2022-11-23 12:54:39 +1100

    fourth try

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   reactStrictMode: true,
   "headers": [
     {
-      "source": "/api/(.*)",
+      "source": "/api/:path*",
       "headers": [
         { "key": "Access-Control-Allow-Credentials", "value": "true" },
         { "key": "Access-Control-Allow-Origin", "value": "*" },
